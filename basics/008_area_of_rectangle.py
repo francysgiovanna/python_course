e8e949f76c25a0e766e23c01923fc5eb8e5455a7
Francys Higuera <francysgiovannahiguerad@gmail.com>
2023-11-12 10:17:43 -0400

    manage dictionaries

---
+++ basics/008_area_of_rectangle.py
@@ -0,0 +1,8 @@
+# calculo del area de un rectangulo 
+print('area de un rectangulo')
+print('ingrese base')
+length = float (input())
+print('ingrese altura')
+width = float (input())
+result = length * width
+print('El area del rectangulo es:  ', result)
---
+++ basics/010_math_funtions.py
@@ -0,0 +1,11 @@
+import math
+# math funtions
+print('raiz cuadrada')
+print('ingrese numero')
+num = float (input())
+result = math.sqrt(num)
+print('la raiz cuadrada de:  ',num,' es igual a: ', result)
+square1=2**2
+print('square 1', square1)
+square2=pow( 2,2)
+print('square 2', square2)
