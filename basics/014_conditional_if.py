c300c717cd687281b00f95ad3dd87b63c65ca86e
Francys Higuera <francysgiovannahiguerad@gmail.com>
2023-12-08 12:12:19 -0400

    conditional

---
+++ basics/014_conditional_if.py
@@ -0,0 +1,16 @@
+cost1 = 20
+cost2 = 30
+if (cost1 > cost2):
+    print('el valor de ',cost1, 'es mayor que ',cost2)
+else:
+    print('el valor de ',cost1, 'es menor que ',cost2)
+
+print('conditional example')
+print('ingrese la edad')
+edad = float (input())
+if (edad == 18):
+    print('acabas de cumplir tu mayoria de edad')
+elif (edad > 17):
+    print('eres mayor de edad')
+else:
+    print('eres menor de edad')
