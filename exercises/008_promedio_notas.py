7ccc9d8a2ea49d9afbb4064aad8f610aaf4b679b
Francys Higuera <francysgiovannahiguerad@gmail.com>
2023-12-11 11:22:06 -0400

    math_exercises

---
+++ exercises/008_promedio_notas.py
@@ -0,0 +1,12 @@
+#Se necesita obtener el promedio simple de un estudiante a partir
+#de sus tres notas parciales N1, N2 y N3.
+#calculo de promedio de 3 notas
+print('calculo de promedio de 3 notas')
+print('calculo de primer parcial')
+parcial1 = float (input())
+print('calculo de segundo parcial')
+parcial2 = float (input())
+print('calculo de tercer parcial')
+parcial3 = float (input())
+promedio = (parcial1 + parcial2 + parcial3)/3
+print('el promedio del alumno es  ', promedio)
